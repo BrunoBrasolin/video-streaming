15dc9689e2ed557b67fe128554cf9a2226773d85
Bruno Brasolin <bruno@otimaideia.com>
2021-02-16 08:49:16 -0300

    starting detail section

--- src/pages/Video/index.js
+++ src/pages/Video/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import Header from "../../components/Header";
+import NetflixLogo from "../../assets/images/Netflix_Logo_RGB.png";
+import "./styles.css";
 
 export default function Video() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <img id="logo" src={NetflixLogo} alt="Netflix" title="Netflix" />
+        <section className="detail">
+          <h2 id="detail__title">Video Title</h2>
+          <div>
+            <span>stars</span>
+            <span>year</span>
+            <span>seasons/duration</span>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis praesentium quia beatae odit labore, est repudiandae. Et earum quos architecto provident, quisquam laborum in id
+            maiores asperiores, perferendis tempore!
+          </p>
+        </section>
+      </main>
+    </>
+  );
 }
