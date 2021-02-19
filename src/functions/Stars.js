20ee41b9de8a3f15843832cb9ba5f52a8400dafa
Bruno Brasolin <bruno@otimaideia.com>
2021-02-19 17:21:17 -0300

    change star function and color

--- src/functions/Stars.js
+++ src/functions/Stars.js
@@ -3,104 +3,114 @@ import EmptyStar from "../assets/icons/empty-star.svg";
 import FullStar from "../assets/icons/full-star.svg";
 
 export default function Stars(score) {
+  if (score === 0)
+    return (
+      <>
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+      </>
+    );
   if (score === 0.5)
     return (
       <>
-        <img src={HalfStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={HalfStar} alt="Half Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 1)
     return (
       <>
-        <img src={FullStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 1.5)
     return (
       <>
-        <img src={FullStar} />
-        <img src={HalfStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={HalfStar} alt="Half Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 2)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 2.5)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={HalfStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={HalfStar} alt="Half Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 3)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={EmptyStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={EmptyStar} alt="Empty Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 3.5)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={HalfStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={HalfStar} alt="Half Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 4)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={EmptyStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={EmptyStar} alt="Empty Star" />
       </>
     );
   if (score === 4.5)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={HalfStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={HalfStar} alt="Half Star" />
       </>
     );
   if (score === 5)
     return (
       <>
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
-        <img src={FullStar} />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
+        <img src={FullStar} alt="Full Star" />
       </>
     );
 }
