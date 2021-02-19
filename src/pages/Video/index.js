4cb613fef99bc85261ee392d663d445ac9834824
Bruno Brasolin <bruno@otimaideia.com>
2021-02-19 17:42:30 -0300

    finish side image

--- src/pages/Video/index.js
+++ src/pages/Video/index.js
@@ -9,19 +9,22 @@ export default function Video() {
     <>
       <Header />
       <main>
-        <img id="logo" src={NetflixLogo} alt="Netflix" title="Netflix" />
-        <section id="detail">
-          <h2 id="detail__title">Video Title</h2>
-          <section id="detail__info">
-            <span title={`${3.5} de 5`}>{Stars(3.5)}</span>
-            <span>2021</span>
-            <span>4 Seasons</span>
+        <section id="content">
+          <img id="logo" src={NetflixLogo} alt="Netflix" title="Netflix" />
+          <section id="detail">
+            <h2 id="detail__title">Video Title</h2>
+            <section id="detail__info">
+              <span title={`${3.5} de 5`}>{Stars(3.5)}</span>
+              <span>2021</span>
+              <span>4 Seasons</span>
+            </section>
+            <p id="detail__about">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis praesentium quia beatae odit labore, est repudiandae. Et earum quos architecto provident, quisquam laborum in id
+              maiores asperiores, perferendis tempore!
+            </p>
           </section>
-          <p id="detail__about">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat facilis praesentium quia beatae odit labore, est repudiandae. Et earum quos architecto provident, quisquam laborum in id
-            maiores asperiores, perferendis tempore!
-          </p>
         </section>
+        <img id="side-image" src="https://source.unsplash.com/300x625/?anime" alt="Random" title="Random" />
       </main>
     </>
   );
