d5ac7e47423ca96b1a4bef69e1fcf331ee4cbb5e
Bruno Brasolin <brunobrasolinc@gmail.com>
2021-02-22 08:30:51 -0300

    fix routes

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Video} />
+        <Route path="/" component={Video} />
       </Switch>
     </BrowserRouter>
   );
